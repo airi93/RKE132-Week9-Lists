5a855176bc54e6d20f4a3023414de7e012ef8ad8
Irina Belik. <ibelik@tktk.ee>
2023-12-26 20:21:47 +0200

    add 9week code

---
+++ 9WeekLists/Program.cs
@@ -0,0 +1,36 @@
+﻿
+ 
+while (true)
+{
+    Console.WriteLine("Add an item (add)/ Exit (exit):");
+    string userChoice = Console.ReadLine();
+
+    if (userChoice == "add")
+    {
+        Console.WriteLine("Enter an item:");
+        string userItem = Console.ReadLine();
+        myShoppingList.Add(userItem);
+    }
+    else 
+    {
+        Console.WriteLine("Bye!");
+        break;
+    }
+}
+}
+
+int listLength = myShoppingList.Count;
+Console.WriteLine($"You have added {listLength} items");
+
+foreach (string item in myShoppingList)
+{ 
+    Console.WriteLine(item); 
+}
+
+int i = 1;
+foreach (string item in myShoppingList) 
+{
+    Console.WriteLine($"{i}.{item}");
+    i++;
+}
+Console.ReadKey();
---
+++ 9WeekLists1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+string folderPath = @"C:\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+
+List<string> myShoppingList = new List<string>();
+
+if (File.Exists(filePath))
+{ 
+myShoppingList = GetItemsFromUser();
+ShowItemsFromList(myShoppingList);
+File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    Console.WriteLine ($"File {fileName} does not exist."); 
+}
+static List<string> GetItemsFromUser() 
+{ 
+    List<string> userList = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("Add an item (add)/ Exit (exit):");
+    string userChoice = Console.ReadLine();
+
+    if (userChoice == "add")
+    {
+        Console.WriteLine("Enter an item:");
+        string userItem = Console.ReadLine();
+        userList.Add(userItem);
+    }
+    else
+    {
+        Console.WriteLine("Bye!");
+        break;
+    }
+}
+return userList;
+}
+
+static void ShowItemsFromList(List<string> someList)
+{ 
+    Console.Clear();
+    int listLength = someList.Count;
+    Console.WriteLine($"You have added {listLength} items to your shopping list.");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+    Console.WriteLine($"{i}.{item}");
+    i++;
+    }
+}
+
+
